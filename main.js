65335b117f946c635d9587d43eac83c7237ccc70
axixnix <henryirons@yahoo.com>
2018-12-12 18:42:16 +0100

    added animation with keyframes did a bit of js to make the displayed image thumbnail on first load

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-const current = document.querySelector('#current');
-const imgs = document.querySelectorAll('.imgs img');
-const opacity = 0.4;
+const current = document.querySelector('#current')
+const imgs = document.querySelectorAll('.imgs img')
+const opacity = 0.4
 
+imgs[0].style.opacity = opacity
 
-imgs.forEach(img => img.addEventListener('click',imgClick));
+imgs.forEach(img => img.addEventListener('click', imgClick))
 
-function imgClick(e){
-    imgs.forEach(img => img.style.opacity =  1);
+function imgClick (e) {
+  imgs.forEach(img => img.style.opacity = 1)
 
+  current.src = e.target.src
 
-    current.src = e.target.src;
+  current.classList.add('fade-in')
 
-    e.target.style.opacity = opacity;
+  setTimeout(() => {
+    current.classList.remove('fade-in')
+  }, 500)
+
+  e.target.style.opacity = opacity
 }
